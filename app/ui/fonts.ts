9d94e1375449906b4d3adf7383a2748c26968392
narendra.jangir@zentekinfosoft.com <naredra.jangir@zentekinfosoft.com>
2025-02-17 21:57:22 -0800

    added font here

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,10 @@
 import { Roboto, Lusitana } from "next/font/google";
 
-export const inter = Roboto({ weight: ["100", "300", "400"] });
-export const lusitana = Lusitana({ weight: ["400", "700"] });
+export const inter = Roboto({
+  weight: ["100", "300", "400"],
+  subsets: ["latin"],
+});
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
